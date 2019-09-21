af7d25e9c9c501d9b2ea70d2a034a84e2ee6379a
unknown <todorbm85@gmail.com>
2019-09-21 14:04:39 +0300

    Update publish script

--- tooling/publish-module.ps1
+++ tooling/publish-module.ps1
@@ -1,1 +1,10 @@
+Param(
+    [Parameter(Mandatory = $true)][string]$version
+)
+
+Set-Location "$PSScriptRoot"
+
+git tag $version
+git push origin --tags
+
 Publish-Module -Name "toko-admin" -NuGetApiKey $Env:NuGetApiKey
